4f75b1459a382488dff6203f1dd51191f6579aa2
tristan.hintz.20@cnu.edu <tristan.hintz.20@cnu.edu>
2023-11-30 17:24:02 -0500

    Updating php

--- html/capacity_tracking.php
+++ html/capacity_tracking.php
@@ -1,41 +1,43 @@
-<!DOCTYPE html>
+<!DOCTYPE html>  
 <html>
+                
 <head>
-<title>Capacity Tracking</title>
+        <title>  
+                Capacity Tracking
+        </title>
 </head>
-<body>
-<?php
-echo "Total Customer Count";
-$count = 0;
-if (array_key_exists('add_customer', $_POST)) {
-	add_customer()
-}
-else if (array_key_exists('remove_customer', $_POST)) {
-	remove_customer()
-}
-else if (array_key_exists('reset_customer', $_POST)) {
-	reset_customer()
-}
-function add_customer() {
-	$count += 1;
-	echo "$count";
-}
-function remove_customer() {
-	$count -= 1;
-	echo "$count";
-}
-function reset_customer() {
-	$count == 0;
-	echo "$count";
-}
-?>
-<form method="post">
-	<input type="submit" name="add_customer"
-		class="button" value="Add Customer" />
-	<input type="submit" name="remove_customer"
-		class="button" value="Remove Customer" />
-	<input type="submit" name="reset_custmoer"
-		class="button" value="Remove Customer" />
-</form>
+                        
+<body style="text-align:center;">   
+                 
+        <h4>
+                Total Customer Count
+        </h4>
+        
+	<?php
+	session_start();
+	
+	if (isset($_POST['button1'])) {
+		$_SESSION['count'] += 1;
+	}
+	else if (isset($_POST['button2'])) {
+		if ($_SESSION['count'] > 0) {
+			$_SESSION['count'] -= 1;
+		}
+	}
+	else if (isset($_POST['button3'])) {
+		$_SESSION['count'] = 0;
+	}
+	
+	echo($_SESSION['count']);
+	?>
+     
+        <form action="" method="post">
+        <input type="submit" name="button1" value="Add Customer">
+	<input type="submit" name="button2" value="Remove Customer">
+	<input type="submit" name="button3" value="Reset Customer">
+        </form>
+	
+	
 </body>
+                
 </html>
