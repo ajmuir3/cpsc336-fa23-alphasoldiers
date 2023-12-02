194ee4a75d8f9b6680684deb0098690dc5e4aca0
kylebeale <150394866+kylebeale@users.noreply.github.com>
2023-12-02 13:11:55 -0500

    Add files via upload
    Updated php file to include connect.php

---
+++ capacity_tracking.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>  
+<html>
+                
+<head>
+        <title>  
+                Capacity Tracking
+        </title>
+</head>
+                        
+<body style="text-align:center;">   
+                 
+        <h4>
+                Total Customer Count
+        </h4>
+        
+	<?php
+
+	include ('connect.php');
+
+	session_start();
+	
+	if (isset($_POST['button1'])) {
+		$_SESSION['count'] += 1;
+	}
+	else if (isset($_POST['button2'])) {
+		if ($_SESSION['count'] > 0) {
+			$_SESSION['count'] -= 1;
+		}
+	}
+	else if (isset($_POST['button3'])) {
+		$_SESSION['count'] = 0;
+	}
+	
+	echo($_SESSION['count']);
+	?>
+     
+        <form action="" method="post">
+        <input type="submit" name="button1" value="Add Customer">
+	<input type="submit" name="button2" value="Remove Customer">
+	<input type="submit" name="button3" value="Reset Customer">
+        </form>
+	
+	
+</body>
+                
+</html>
